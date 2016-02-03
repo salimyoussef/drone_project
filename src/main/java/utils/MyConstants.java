536709e32e4a6657ee54bc9b18289115d84931d1
Youssef Salim <salimyoussef11@yahoo.fr>
2016-02-04 00:17:34 +0100

    Merge

--- src/main/java/utils/MyConstants.java
+++ src/main/java/utils/MyConstants.java
@@ -6,11 +6,7 @@ package utils;
 public class MyConstants {
     public static final int KAFKA_ZK_PORT = 2181;
     public static final int INITIAL_BROKER_PORT = 9092;
-<<<<<<< HEAD
     public static final int NUMBER_OF_DRONES = 10;
-=======
-    public static final int NUMBER_OF_DRONES = 2;
->>>>>>> 8fbcbde522041e9847afaba470f85618514619c5
     public static final int NUMBER_OF_BROKERS = 1;
     public static final int NUMBER_OF_ZOOKEEPER = 1;
 }
